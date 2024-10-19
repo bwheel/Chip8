ad9646dc33be3a7cd3c9bd127abc5d907a0a9087
Byron Wheeler <bwheel1990@gmail.com>
2024-10-19 15:16:22 -0500

    add basic assembler

---
+++ src/Chip8.Assembler/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+
+namespace Chip8.Assembler;
+
+
+public class Program
+{
+
+
+
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0 || string.IsNullOrWhiteSpace(args[0]))
+        {
+            Console.WriteLine("Please provide a file to compile");
+            Environment.Exit(1);
+        }
+
+        string filename = args[0];
+        if (!File.Exists(filename))
+        {
+            Console.WriteLine($"argument: {filename} does not exist");
+            Environment.Exit(1);
+        }
+
+        using var fileStream = File.OpenRead(filename);
+        using var reader = new StreamReader(fileStream);
+
+        List<string> commands = new List<string>();
+        string? line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            // empty line.
+            if (string.IsNullOrWhiteSpace(line))
+                continue;
+            // commented out line.
+            if (line.TrimStart().StartsWith("#"))
+                continue;
+        }
+    }
+}
